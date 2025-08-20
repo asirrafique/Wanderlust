7990a41e7c3a386dbe0df59bec6438d9d860af61
asirrafique <asirrafique1@gmail.com>
2025-08-21 02:46:22 +0530

    Express Sessions

--- schema.js
+++ schema.js
@@ -1,5 +1,6 @@
 const Joi = require("joi");
 const Listing = require("./models/listing");
+const review = require("./models/review");
 
 module.exports.listingSchema = Joi.object({
   listing: Joi.object({
@@ -9,5 +10,12 @@ module.exports.listingSchema = Joi.object({
     country: Joi.string().required(),
     price: Joi.number().required().min(0),
     image: Joi.string().allow("", null),
+  }).required(),
+});
+
+module.exports.reviewSchema = Joi.object({
+  review: Joi.object({
+    rating: Joi.number().required().min(1).max(5),
+    comment: Joi.string().required(),
   }).required(),
 });
