4abd27efda9755eba9964ff490ea16b744bf2ac2
asirrafique <asirrafique1@gmail.com>
2025-08-21 17:08:08 +0530

    Authorization added

--- init/index.js
+++ init/index.js
@@ -7,19 +7,23 @@ const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 main()
   .then(() => {
     console.log("connected to DB");
-})
+  })
   .catch((err) => {
     console.log(err);
-});
+  });
 
 async function main() {
-    await mongoose.connect(MONGO_URL);
+  await mongoose.connect(MONGO_URL);
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    await Listing.insertMany(initData.data);
-    console.log("data was initialized");
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "68a6dfa70f3e2d7aa28981c1",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data was initialized");
 };
 
-initDB();   
+initDB();
